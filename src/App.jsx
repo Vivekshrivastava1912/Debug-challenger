bbdc9b614f7b1b4bdd22558691da2330f5b61eb0
Vivekshrivastava1912 <shrivastavavivek19d02005@gmail.com>
2025-07-10 23:50:41 +0530

    landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
 import React from 'react'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+import Challenge from './components/Challenge'
+import Nav from './components/Nav'
+import Landing from './components/Landing'
 
-export default function App() {
+const App = () => {
   return (
-    <header className="flex items-center justify-between px-6 py-4 bg-gray-900 text-white shadow-md">
-      <h1 className="text-xl font-bold">🔥 My Clerk Auth App</h1>
-
-      <nav>
-        <SignedOut>
-          <SignInButton mode="modal">
-            <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-md">
-              Sign In
-            </button>
-          </SignInButton>
-        </SignedOut>
-
-        <SignedIn>
-          <UserButton afterSignOutUrl="/" />
-        </SignedIn>
-      </nav>
-    </header>
-  );
+    <>
+    <Nav/>
+  <Landing/>
+    </>
+  )
 }
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+const Nav = () => {
+  return (
+    <>
+     <header className="flex items-center justify-between px-6 py-4 bg-gray-900 text-white shadow-md">
+      <h1 className="text-xl font-bold">🔥 My Clerk Auth App</h1>
+
+      <nav>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-md">
+              Sign In
+            </button>
+          </SignInButton>
+        </SignedOut>
+
+        <SignedIn>
+          <UserButton afterSignOutUrl="/" />
+        </SignedIn>
+      </nav>
+    </header></>
+  )
+}
+
+export default Nav
